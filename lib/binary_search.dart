c5041bcf5605a3cbc190513afbae80ed7a35c76c
Mohsen Hashemi <m.hashemi.code@gmail.com>
2023-05-02 07:41:15 +0330

    implement binary search as a free function

--- lib/binary_search.dart
+++ lib/binary_search.dart
@@ -19,16 +19,38 @@ extension SortedList<E extends Comparable<dynamic>> on List<E> {
     // Find the middle index of the range.
     final size = endIndex - startIndex;
     final middle = startIndex + size ~/ 2;
-    
+
     // Do the binary search recursively till the middle point matches the given
     // value and return its [index] or the last condition met and return [null].
     if (this[middle] == value) {
       return middle;
-    }
-    else if (value.compareTo(this[middle]) < 0) {
+    } else if (value.compareTo(this[middle]) < 0) {
       return binarySearch(value, startIndex, middle);
     } else {
       return binarySearch(value, middle + 1, endIndex);
     }
+  }
+}
+
+// Implement binary search as a free function
+int? binarySearch<E extends Comparable<dynamic>>(
+  List<E> list,
+  E value, [
+  int? start,
+  int? end,
+]) {
+  final startIndex = start ?? 0;
+  final endIndex = end ?? list.length;
+  if (startIndex >= endIndex) return null;
+  
+  final size = endIndex - startIndex;
+  final middle = startIndex + size ~/ 2;
+
+  if (list[middle] == value) {
+    return middle;
+  } else if (value.compareTo(list[middle]) < 0) {
+    return binarySearch(list, value, startIndex, middle);
+  } else {
+    return binarySearch(list, value, middle + 1, endIndex);
   }
 }
